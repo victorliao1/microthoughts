244a56e6a2e3fad1c1dbc04364074f53386cb5c2
Mark Zhang <markzhang.inbox@gmail.com>
2021-05-07 18:44:46 -0400

    updated render.py

--- render.py
+++ render.py
@@ -1,3 +1,6 @@
-file = open('output/index.html', 'w+')
-file.write("Hello world!")
-file.close()
+import os
+print(os.getcwd())
+
+# file = open('output/index.html', 'w+')
+# file.write("Hello world!")
+# file.close()
