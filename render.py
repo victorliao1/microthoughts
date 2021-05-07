d3303ab4aa49c1c6eaedd4991b1c75af7010ccb1
Mark Zhang <markzhang.inbox@gmail.com>
2021-05-07 18:48:18 -0400

    updated render.py

--- render.py
+++ render.py
@@ -1,6 +1,6 @@
 import os
 print(os.listdir(os.curdir))
 
-# file = open('output/index.html', 'w+')
-# file.write("Hello world!")
-# file.close()
+file = open('index.html', 'w+')
+file.write("Hello world!")
+file.close()
