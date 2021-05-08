96a7a113cf7374c0d3d6d74095a0f2db9ffc5265
Mark Zhang <mark.zhang@uwaterloo.ca>
2021-05-07 23:05:33 -0400

    Implemented static site generator

--- render.py
+++ render.py
@@ -1,9 +1,31 @@
 import os
-from time import time
+from datetime import datetime
 import markdown
 
-print(os.listdir(os.curdir))
+class Renderer:
 
-file = open('output/index.html', 'w+')
-file.write(str(markdown.markdown('#Hi')))
-file.close()
+    def __init__(self):
+        with open("templates/page.html") as f:
+            self.page_template = f.read()
+        with open("templates/post.html") as f:
+            self.post_template = f.read()
+        self.render()
+
+    def render(self):
+        posts = []
+        for file in reversed(os.listdir("thoughts")):
+            if file.endswith(".md"):
+                with open("thoughts/" + file, "r", encoding="utf-8") as f:
+                    text = f.read()
+                text_html = markdown.markdown(text, extensions=['nl2br', 'tables'])
+                raw_timestamp = file.split(".")[0]
+                date = datetime.strptime(raw_timestamp, "%Y%m%d")
+                date_str = date.strftime("%a, %b %d, %Y")
+                post_html = self.post_template.format(content=text_html, timestamp=date_str)
+                posts.append(post_html)
+        page_html = self.page_template.format(posts="".join(posts))
+        with open('output/index.html', 'w+') as f:
+            f.write(page_html)
+
+if __name__ == '__main__':
+    Renderer()
